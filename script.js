025315611821db4d479c2f584efc3b8d7af5d1c5
Anastasia Beskishkova <anastasi.verner@yandex.ru>
2023-12-27 12:23:49 +0300

    внесение корректировок, добавление роутинга

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-// URL для API
+// URL для API 
 const apiBaseUrl = 'https://skylord.ru/test';
 
-// Новый экземпляр Vue
-const app = new Vue({
-    // Элемент, к которому привязан Vue
-    el: '#app',
-
-    // Данные
-    data: {
-        items: [], // Массив элементов
-        newItem: { // Объект нового элемента
-            title: '',
-            text: '',
-            status: 0,
-        },
+// Компонент списка элементов 
+const ItemList = {
+    template: `
+        <div class="box">
+            <h1>Список добавленных элементов</h1>
+            <div v-for="item in items" :key="item.id" class="box">
+                <div class="item_box">
+                    <div class="texts"> 
+                        <p>{{ userFriendlyIndex(item) }}.</p> 
+                        <p>{{ item.title }}</p>  
+                    </div>     
+                    <div class="buttons">       
+                        <router-link class="button" :to="'/item/' + item.id">Показать описание</router-link>
+                        <button class="button" @click="deleteItem(item.id)">Удалить</button>
+                    </div> 
+                </div>
+            </div>            
+            <div class="box">
+                <h2>Добавьте новый элемент</h2>                
+                <item-form @submit="addItem"></item-form>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            items: [],
+        };
     },
-
-    // Методы, которые содержат логику приложения
     methods: {
-        // Получение элементов с сервера
+        // Получение элементов с сервера 
         fetchItems() {
-            // Выполняем GET-запрос к API для получения списка элементов
             axios.get(`${apiBaseUrl}/`)
                 .then(response => {
-                    // Для каждого элемента из ответа формируем запрос на получение деталей элемента
                     const requests = response.data.map(item => {
                         return axios.get(`${apiBaseUrl}/${item.id}`);
                     });
 
-                    // Одновременно выполняем все запросы к деталям элементов
                     axios.all(requests)
                         .then(responses => {
-                            // Заполняем массив элементами
                             this.items = responses.map(response => {
                                 const item = response.data;
-                                item.showDetails = false; 
+                                item.showDetails = false;
                                 return item;
                             });
                         })
@@ -46,36 +54,17 @@ const app = new Vue({
                     console.error('Error fetching items:', error);
                 });
         },
-
-        // Переключение видимости описания элемента
-        toggleItemDetails(item) {
-            item.showDetails = !item.showDetails;
-        },
-
-        // Добавление нового элемента
-        addItem() {
-            axios.post(`${apiBaseUrl}/`, this.newItem)
+        // Добавление нового элемента 
+        addItem(newItem) {
+            axios.post(`${apiBaseUrl}/`, newItem)
                 .then(response => {
                     this.fetchItems();
-                    this.newItem = {}; // Очищаем форму после добавления
                 })
                 .catch(error => {
                     console.error('Error adding item:', error);
                 });
         },
-
-        // Обновление элемента
-        updateItem(item) {
-            axios.put(`${apiBaseUrl}/${item.id}`, item)
-                .then(response => {
-                    this.fetchItems();
-                })
-                .catch(error => {
-                    console.error('Error updating item:', error);
-                });
-        },
-
-        // Удаление элемента
+        // Удаление элемента 
         deleteItem(itemId) {
             axios.delete(`${apiBaseUrl}/${itemId}`)
                 .then(response => {
@@ -85,65 +74,124 @@ const app = new Vue({
                     console.error('Error deleting item:', error);
                 });
         },
-    },
-
-    // Вычисляемые свойства для использования в шаблоне
-    computed: {
-        // Функция для отображения пользовательского индекса элемента
-        userFriendlyIndex() {
-            return item => this.items.findIndex(i => i === item) + 1;
+        // Функция для отображения пользовательского индекса элемента 
+        userFriendlyIndex(item) {
+            return this.items.findIndex(i => i === item) + 1;
         },
     },
-
-    // Хук жизненного цикла Vue, вызывается после создания экземпляра
+    // Хук жизненного цикла Vue, вызывается после создания экземпляра 
     mounted() {
-        this.fetchItems(); // Вызываеся при загрузке страницы
+        this.fetchItems();
     },
+};
 
-    // Шаблон Vue с разметкой страницы
-    template: `
-    <div class="box">
-        <h1>Список добавленных элементов</h1>
-        <div v-for="item in items" :key="item.id" class="box">
-            <div class="item_box">
-                <div class="texts"> 
-                    <p>{{ userFriendlyIndex(item) }}.</p> 
-                    <p>{{ item.title }}</p>  
-                </div>     
-                <div class="buttons">       
-                    <button @click="toggleItemDetails(item)">{{ item.showDetails ? 'Скрыть описание' : 'Показать описание' }}</button>
-                    <button @click="deleteItem(item.id)">Удалить</button>
-                </div> 
-            </div>
-            <div v-if="item.showDetails" class="descriptions">
-                <label>Title: <input v-model="item.title"></label>
-                <label>Text: <input v-model="item.text"></input></label>
-                <label>Status:
-                    <select v-model="item.status">
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                    </select>
-                </label>
-                <button @click="updateItem(item)" class="color">Обновить элемент</button>
-            </div>
-        </div>
-        
+// Компонент просмотра элемента 
+const ItemDetails = {
+    template: `    
         <div class="box">
-        <h2>Добавьте новый элемент</h2>
-            <form @submit.prevent="addItem" class="descriptions">
-                <label for="newItemTitle">Title: <input id="newItemTitle" v-model="newItem.title" required></label>
-                <label for="newItemText">Text: <input id="newItemText" v-model="newItem.text" required></input></label>
-                <label for="newItemStatus">Status:
-                    <select id="newItemStatus" v-model="newItem.status">
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                    </select>
-                </label>
-                <button type="submit" class="color">Добавить</button>
-            </form>
+            <h1>Просмотр элемента</h1>  
+            <item-form :initialItem="item" @submit="updateItem"></item-form>            
+            <router-link class="button" to="/">Назад</router-link>
         </div>
-    </div>
-`,
+    `,
+    data() {
+        return {
+            item: null,
+        };
+    },
+    methods: {
+        // Получение данных элемента с сервера 
+        fetchItem() {
+            axios.get(`${apiBaseUrl}/${this.$route.params.id}`)
+                .then(response => {
+                    this.item = response.data;
+                })
+                .catch(error => {
+                    console.error('Error fetching item:', error);
+                });
+        },
+        // Обновление элемента 
+        updateItem(item) {
+            axios.put(`${apiBaseUrl}/${item.id}`, item)
+                .then(response => {
+                    console.log('Item updated successfully:', response.data);
+                })
+                .catch(error => {
+                    console.error('Error updating item:', error);
+                });
+        },
+    },
+    // Хук жизненного цикла Vue, вызывается при создании компонента 
+    created() {
+        this.fetchItem();
+    },
+    // Watcher для отслеживания изменений параметра маршрута 
+    watch: {
+        '$route.params.id': 'fetchItem',
+    },
+};
+
+// Компонент формы редактирования элемента 
+Vue.component('item-form', {
+    props: ['initialItem'],
+    template: `
+        <form @submit.prevent="submitForm" class="descriptions">
+            <label>Title: <input v-model="newItem.title" required></label>
+            <label>Text: <input v-model="newItem.text" required></input></label>
+            <label>Status:
+                <select v-model="newItem.status">
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+            </label>
+            <button type="submit" class="button color">{{ buttonText }}</button>
+        </form>
+    `,
+    data() {
+        return {
+            newItem: { ...this.initialItem }, // Инициализировать newItem значениями из initialItem 
+        };
+    },
+    computed: {
+        buttonText() {
+            return this.initialItem ? 'Обновить элемент' : 'Добавить';
+        },
+    },
+    // Watcher для отслеживания изменений initialItem 
+    watch: {
+        initialItem: {
+            handler(value) {
+                // При изменении initialItem обновляем newItem 
+                this.newItem = { ...value };
+            },
+            immediate: true, // Запускать обработчик при создании компонента 
+        },
+    },
+    methods: {
+        submitForm() {
+            this.$emit('submit', this.newItem);
+        },
+    },
+});
+
+// Создание экземпляра маршрутизатора 
+const router = new VueRouter({
+    routes: [{
+            path: '/',
+            component: ItemList
+        },
+        {
+            path: '/item/:id',
+            component: ItemDetails,
+            props: true
+        },
+    ],
+});
+
+// Новый экземпляр Vue с маршрутизатором 
+const app = new Vue({
+    el: '#app',
+    router,
+    template: '<router-view></router-view>',
 });
